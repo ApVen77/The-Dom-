6b243d816995f735699c7179a8546eff017539a8
April Long <april11dance@gmail.com>
2022-01-19 21:20:11 -0700

    cleaned up files for class

--- app.js
+++ app.js
@@ -1,28 +1,80 @@
-// Start with this HTML and save it as "about_me.html":
+document.addEventListener('DOMContentLoaded', function(){
+
+    const list = document.querySelector('#quote-list ul');
+    const forms = document.forms;
   
-// Add a script tag to the bottom of the HTML body.//
-// (In the JavaScript) Change the body tag's style so it has a font-family of "Arial, sans-serif".
-// (In the JavaScript) Replace each of the spans (nickname, favorites, hometown) with your own information.
-// Iterate through each li and change the class to "list-item".
-// (In the HTML head) Add a style tag that sets a rule for .list-item to make the color red.
-// Create a new img element and set its src attribute to a picture of you. Append that element to the page.
-//innerHTMl modifies text(old way)  textcontent (more secure less browser)
-document.body.style.fontFamily= 'Arial, Sans-serif';
-
-document.body.style.fontSize= '40px'; 
-document.getElementById('nickname').textContent= "Woman"; 
-document.getElementById('favorites').textContent= "Politics"; 
-document.getElementById('hometown').textContent= "Milwaukee"; 
-document.getElementById('interests').textContent= "Kids, Fun, and Sun"; 
-
-var items= document.getElementsByTagName('li'); 
-for (let i= 0; i<items.length; i++) {
-    items[i]. className= 'list-item'; 
-}
-
-var picture= document.createElement('img');
-picture.src= 'https://ca.slack-edge.com/T018R8C5LSE-U018XDUCS12-7235bcf7e011-512'
-picture.style.width= '250px';
-picture.style.height= '200px';
-picture.style.border= " 2px black solid"; 
-document.body.appendChild(picture); 
+    // delete quotes
+    list.addEventListener('click', (e) => {
+      if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        li.parentNode.removeChild(li);
+      }
+    });
+  
+    // add quotes
+    const addForm = forms['add-quote'];
+    addForm.addEventListener('submit', function(e){
+      e.preventDefault();
+  
+      // create elements
+      const value = addForm.querySelector('input[type="text"]').value;
+      const li = document.createElement('li');
+      const quoteName = document.createElement('span');
+      const deleteBtn = document.createElement('span');
+  
+      // add text content
+      quoteName.textContent = value;
+      deleteBtn.textContent = 'delete';
+  
+      // add classes
+      quoteName.classList.add('name');
+      deleteBtn.classList.add('delete');
+  
+      // append to DOM
+      li.appendChild(quoteName);
+      li.appendChild(deleteBtn);
+      list.appendChild(li);
+    });
+  
+    // hide quotes
+    const hideBox = document.querySelector('#hide');
+    hideBox.addEventListener('change', function(e){
+      if(hideBox.checked){
+        list.style.display = "none";
+      } else {
+        list.style.display = "initial";
+      }
+    });
+  
+    // filter quotes
+    const searchBar = forms['search-quotes'].querySelector('input');
+    searchBar.addEventListener('keyup', (e) => {
+      const term = e.target.value.toLowerCase();
+      const quotes = list.getElementsByTagName('li');
+      Array.from(quotes).forEach((quotes) => {
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(e.target.value) != -1){
+          quotes.style.display = 'block';
+        } else {
+          quotes.style.display = 'none';
+        }
+      });
+    });
+  
+    // tabbed content
+    const tabs = document.querySelector('.tabs');
+    const panels = document.querySelectorAll('.panel');
+    tabs.addEventListener('click', (e) => {
+      if(e.target.tagName == 'LI'){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        Array.from(panels).forEach((panel) => {
+          if(panel == targetPanel){
+            panel.classList.add('active');
+          }else{
+            panel.classList.remove('active');
+          }
+        });
+      }
+    });
+  
+  })
--- form.js
+++ form.js
@@ -1,203 +1,102 @@
-// // Defining a function to display error message
-// function printError(elemId, hintMsg) {
-//     document.getElementById(elemId).innerHTML = hintMsg;
-// }
+console.log("JS is Alive!")
 
-// // Defining a function to validate form 
-// function validateForm() {
-//     // Retrieving the values of form elements 
-//     var name = document.contactForm.name.value;
-//     var email = document.contactForm.email.value;
-//     var mobile = document.contactForm.mobile.value;
-//     var country = document.contactForm.country.value;
-//     var gender = document.contactForm.gender.value;
-//     var hobbies = [];
-//     var checkboxes = document.getElementsByName("hobbies[]");
-//     for (var i = 0; i < checkboxes.length; i++) {
-//         if (checkboxes[i].checked) {
-//             // Populate hobbies array with selected values
-//             hobbies.push(checkboxes[i].value);
-//         }
-//     }
+// Defining a function to display error message
+function printError(elemId, hintMsg) {
+    document.getElementById(elemId).innerHTML = hintMsg;
+}
 
-//     // Defining error variables with a default value
-//     var nameErr = emailErr = mobileErr = countryErr = genderErr = true;
+// Defining a function to validate form 
+function validateForm() {
+    // Retrieving the values of form elements 
+    var name = document.contactForm.name.value;
+    var email = document.contactForm.email.value;
+    var mobile = document.contactForm.mobile.value;
+    var country = document.contactForm.country.value;
+    var gender = document.contactForm.gender.value;
+    var hobbies = [];
+    var checkboxes = document.getElementsByName("hobbies[]");
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            // Populate hobbies array with selected values
+            hobbies.push(checkboxes[i].value);
+        }
+    }
+    
 
-//     // Validate name
-//     if (name == "") {
-//         printError("nameErr", "Please enter your name");
-//     } else {
-//         var regex = /^[a-zA-Z\s]+$/;
-//         if (regex.test(name) === false) {
-//             printError("nameErr", "Please enter a valid name");
-//         } else {
-//             printError("nameErr", "");
-//             nameErr = false;
-//         }
-//     }
+    // Defining error variables with a default value
+    var nameErr = emailErr = mobileErr = countryErr = genderErr = true;
 
-//     // Validate email address
-//     if (email == "") {
-//         printError("emailErr", "Please enter your email address");
-//     } else {
-//         // Regular expression for basic email validation
-//         var regex = /^\S+@\S+\.\S+$/;
-//         if (regex.test(email) === false) {
-//             printError("emailErr", "Please enter a valid email address");
-//         } else {
-//             printError("emailErr", "");
-//             emailErr = false;
-//         }
-//     }
+    // Validate name
+    if (name == "") {
+        printError("nameErr", "Please enter your name");
+    } else {
+        var regex = /^[a-zA-Z\s]+$/;
+        if (regex.test(name) === false) {
+            printError("nameErr", "Please enter a valid name");
+        } else {
+            printError("nameErr", "");
+            nameErr = false;
+        }
+    }
 
-//     // Validate mobile number
-//     if (mobile == "") {
-//         printError("mobileErr", "Please enter your mobile number");
-//     } else {
-//         var regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-//         if (regex.test(mobile) === false) {
-//             printError("mobileErr", "Please enter a valid 10 digit mobile number");
-//         } else {
-//             printError("mobileErr", "");
-//             mobileErr = false;
-//         }
-//     }
+    // Validate email address
+    if (email == "") {
+        printError("emailErr", "Please enter your email address");
+    } else {
+        // Regular expression for basic email validation
+        var regex = /^\S+@\S+\.\S+$/;
+        if (regex.test(email) === false) {
+            printError("emailErr", "Please enter a valid email address");
+        } else {
+            printError("emailErr", "");
+            emailErr = false;
+        }
+    }
 
-//     // Validate country
-//     if (country == "Select") {
-//         printError("countryErr", "Please select your country");
-//     } else {
-//         printError("countryErr", " ");
-//         countryErr = false;
-//     }
+    // Validate mobile number
+    if (mobile == "") {
+        printError("mobileErr", "Please enter your mobile number");
+    } else {
+        var regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+        if (regex.test(mobile) === false) {
+            printError("mobileErr", "Please enter a valid 10 digit mobile number");
+        } else {
+            printError("mobileErr", "");
+            mobileErr = false;
+        }
+    }
 
-//     // Validate gender
-//     if (gender == " ") {
-//         printError("genderErr", "Please select your gender");
-//     } else {
-//         printError("genderErr", " ");
-//         genderErr = false;
-//     }
+    // Validate country
+    if (country == "Select") {
+        printError("countryErr", "Please select a country from thr List");
+    } else {
+        printError("countryErr", " ");
+        countryErr = false;
+    }
 
-//     // Prevent the form from being submitted if there are any errors
-//     if ((nameErr || emailErr || mobileErr || countryErr || genderErr) == true) {
-//         return false;
-//     } else {
-//         // Creating a string from input data for preview
-//         var dataPreview = "You've entered the following details: \n" +
-//             "Full Name: " + name + "\n" +
-//             "Email Address: " + email + "\n" +
-//             "Mobile Number: " + mobile + "\n" +
-//             "Country: " + country + "\n" +
-//             "Gender: " + gender + "\n";
-//         if (hobbies.length) {
-//             dataPreview += "Hobbies: " + hobbies.join(", ");
-//         }
-//         // Display input data in a dialog box before submitting the form
-//         alert(dataPreview);
-//     }
-// };
+    // Validate gender
+    if (gender == " ") {
+        printError("genderErr", "Please Make a Selection");
+    } else {
+        printError("genderErr", " ");
+        genderErr = false;
+    }
 
-// //create a function called for madlibs
-// function libIts() {
-//     //target the values in the HTMl
-
-//     var storyDiv = document.getElementById('story');
-//     var person = document.getElementById('person').value;
-//     var adjective = document.getElementById('adjective').value;
-//     var noun = document.getElementById('noun').value;
-//     storyDiv.innerHTML = person + "is locked down" + adjective + " " + noun + " Wow this is great!";
-// }
-
-// // var libButton= document.getElementById('lib-button'); 
-// // libButton.addEventListener('click', libIts); 
-
-// // //Add a empty div so when you click on it, the date displays...
-// // function dateToday() {
-// //     document.getElementsByClassName('time').innerHTML= Date(); 
-// // }
-
-// // function style() 
-// // {
-// // //font styles added by JS:
-// //  text.style.fontSize = "14pt";
-// //  text.style.fontFamily = "Comic Sans MS";
-// //  text.style.color = "green";
-// // }
-
-// // function style2() {
-// //     text.style.color= "red";
-// // }
-
-// // function style3() {
-// //     text.style.color= "blue";
-// //     text.style.opacity= '1'
-// // }
-
-// // function style4() {
-// //     text.style.color= "yellow";
-// // }
-
-// // function style5() {
-// //     text.style.color= "pink";
-// //     text.style.fontSize= '50px';
-// // }
-
-// function mOver(obj) {
-//     obj.innerHTML = "Thank You"
-// }
-
-// function mOut(obj) {
-//     obj.innerHTML = "Mouse Over Me"
-// }
-
-// function mOver1(obj) {
-//     obj.innerHTML = "Albany"
-// }
-
-// function mOut1(obj) {
-//     obj.innerHTML = "What is the capital of NY?"
-// }
-
-// function changeColor() {
-//     document.querySelector("button").style.backgroundColor = "pink"; //change background color
-//     document.querySelector("button").style.color = "black "; //change font color
-//     document.querySelector("p").innerHTML = "Great! The button changed its color." //add text
-//   }
-
-// var btn = document.querySelector("button");
-// function changeColor() {
-//     btn.style.backgroundColor = "pink"; //change background color
-//     btn.style.color = "black"; //change font color
-//     document.querySelector("p").innerHTML = "Great! The button changed its color." //add text
-// }
-// btn.onclick = changeColor; //event handler
-
-// var btn = document.querySelector("button");
-// function changeColor() {
-//     btn.style.backgroundColor = "pink"; //change background color
-//     btn.style.color = "black"; //change font color
-//     document.querySelector("p").innerHTML = "Look! The button changed color." //add text
-// }
-// btn.addEventListener("click", changeColor); //adds event listener
-
-// var btn = document.querySelector("button");
-// function changeColor(anyParameter){ //function declaration
-//   btn.style.backgroundColor = "pink"; //change background color
-//   btn.style.color = "black"; //change font color
-//   document.querySelector("p").innerHTML = "Great! The button changed its color." //add text
-// }
-// btn.addEventListener("click", function(){
-//   changeColor(1); //function with a parameter
-// }, false); //adds event listener with anonymous function
-
-// // document.getElementById("demo").onmouseover = function() {mouseOver()};
-// // document.getElementById("demo").onmouseout = function() {mouseOut()};
-
-// // function mouseOver() {
-// //   document.getElementById("demo").style.color = "red";
-// // }
-
-// // function mouseOut() {
-// //   document.getElementById("demo").style.color = "black";
-// // }
+    // Prevent the form from being submitted if there are any errors
+    if ((nameErr || emailErr || mobileErr || countryErr || genderErr) == true) {
+        return false;
+    } else {
+        // Creating a string from input data for preview
+        var dataPreview = "You've entered the following details: \n" +
+            "Full Name: " + name + "\n" +
+            "Email Address: " + email + "\n" +
+            "Mobile Number: " + mobile + "\n" +
+            "Country: " + country + "\n" +
+            "Gender: " + gender + "\n";
+        if (hobbies.length) {
+            dataPreview += "Hobbies: " + hobbies.join(", ");
+        }
+        // Display input data in a dialog box before submitting the form
+        alert(dataPreview);
+    }
+};
